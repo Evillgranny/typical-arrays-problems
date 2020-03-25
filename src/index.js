2924a10d1a92ce20d947f3170127214ca448199b
Evillgranny <44546756+Evillgranny@users.noreply.github.com>
2020-03-26 02:11:10 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
+exports.min = function min (array = 0) {
+    if (array.length === 0 || !array) {
+        return array
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let minNumber = 0
+
+    for (let i = 0; i <= array.length; i++) {
+        if (i === 0) {
+            minNumber = array[i]
+        } else {
+            if (minNumber > array[i]) {
+                minNumber = array[i]
+            }
+        }
+    }
+
+    return minNumber
+};
+
+exports.max = function max (array = 0) {
+    if (array.length === 0 || !array) {
+        return array
+    }
+
+    let maxNumber = 0
+
+    for (let i = 0; i <= array.length; i++) {
+        if (i === 0) {
+            maxNumber = array[i]
+        } else {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i]
+            }
+        }
+    }
+
+    return maxNumber
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg (array = 0) {
+    if (array.length === 0 || !array) {
+        return array
+    }
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    return sum / array.length
 }
